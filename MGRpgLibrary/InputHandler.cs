e7a707c8644ec0d730a79604130fa83bc0baecf1
Alex Humphries <humphriesa02@gmail.com>
2022-01-03 15:53:27 -0800

    Add project files.

---
+++ MGRpgLibrary/InputHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MGRpgLibrary
+{
+    class InputHandler : Microsoft.Xna.Framework.GameComponent
+    {
+        #region Field Region
+        static KeyboardState keyboardState;
+        static KeyboardState lastKeyboardState;
+        #endregion
+
+        #region Property Region
+        public static KeyboardState KeyboardState
+        {
+            get { return keyboardState; }
+        }
+
+        public static KeyboardState LastKeyboardState
+        {
+            get { return lastKeyboardState; }
+        }
+    }
+}
